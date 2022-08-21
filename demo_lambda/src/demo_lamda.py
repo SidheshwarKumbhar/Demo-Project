3826cda45d91b3f5c7dbd75a6e6c8b7678592315
Siddeshwar <siddeshwar-pandit.kumbhar@capgemini.com>
2022-08-21 14:11:19 +0530

    logging commit

--- demo_lambda/src/demo_lamda.py
+++ demo_lambda/src/demo_lamda.py
@@ -1,21 +1,42 @@
 import boto3
+import logging
+
+logger = logging.getLogger("demo_lambda")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("demo_lamda.log", mode='w')
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+logger.info("log for demo_lambda started..")
+
 
 # Lambda Handler
 def lambda_handler(event):
     if event is not None:
         try:
             name = event['Name']
+            logger.info("lambda is triggerd by event {}".format(name))
+
             number = event['Number']
+            logger.info("lambda is triggerd by event {}".format(number))
+
+            logger.info("Connecting with DynamoDB")
             return connecting_dynamodb(name, number)
+
         except KeyError as k:
-            raise KeyError("Error While Connecting to Dynamo DB")
+            # raise KeyError("Error While Connecting to Dynamo DB")
+            logger.error("Error While Connecting to Dynamo DB", k)
 
 
 # Connecting to DynamoDB
 def connecting_dynamodb(name, number):
     try:
         database = boto3.resource('dynamodb')
-        table = database.Table("PhDirectory")    # Table for Phone Directory
+        logger.info("Requested for DynamoDb Services")
+
+        table = database.Table("PhDirectory")  # Table for Phone Directory
+        logger.info("A {} Table is Created".format(table))
 
         elements = table.put_key(
             key={
@@ -23,7 +44,9 @@ def connecting_dynamodb(name, number):
                 'Number': number
             }
         )
+        logger.info("Attributes {} and {} Added to the table".format(name, number))
         return elements
 
     except Exception as e:
-        raise Exception("Error While Connecting to Dynamo DB")
+        # raise Exception("Error While Connecting to Dynamo DB")
+        logger.error("Error While Connecting to Dynamo DB", e)
--- demo_lambda/test/test_lambda.py
+++ demo_lambda/test/test_lambda.py
@@ -2,6 +2,15 @@ import unittest
 from unittest import mock
 from demo_lambda.src.demo_lamda import *
 
+logger = logging.getLogger("test_lambda")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("test_lamda.log", mode='w')
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+logger.info("log for test_lambda started..")
+
 # Test Class
 class TestDemoLambda(unittest.TestCase):
     # Setup
@@ -10,10 +19,7 @@ class TestDemoLambda(unittest.TestCase):
             'Name': "sid",
             'Number': 1234567890
         }
-
-    # Tear Down
-    def tearDown(self):
-        self.event = None
+    logger.info("Values for event is set up")
 
     # Test case for successful run
     def test_lambda(self):
@@ -21,6 +27,7 @@ class TestDemoLambda(unittest.TestCase):
                         return_value={'Name': 'Sid', 'Number': 1234567890}):
             response = lambda_handler(self.event)
         self.assertEqual(response, {'Name': 'Sid', 'Number': 1234567890})
+    logger.info("test case for the successful run completed.")
 
     # Test case for Unsuccessful Run
     def test_lambda_for_failure(self):
@@ -28,6 +35,13 @@ class TestDemoLambda(unittest.TestCase):
                         return_value={'Name': 'Sidheshwar', 'Number': 1234567890}):
             response = lambda_handler(self.event)
         self.assertNotEqual(response, {'Name': 'Sid', 'Number': 1234567890})
+    logger.info("test case for Unsuccessful run completed")
+
+    # Tear Down
+    def tearDown(self):
+        self.event = None
+
+    logger.info("Values of event is tear Down")
 
 
 if __name__ == '__main__':
